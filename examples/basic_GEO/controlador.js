68c821affcda3ca6f94ee1660238ed067219e24a
falbueraPCT <francesc.albuera@pct-turisme.cat>
2018-11-26 14:03:06 +0100

    asdf

--- examples/basic_GEO/controlador.js
+++ examples/basic_GEO/controlador.js
@@ -194,7 +194,15 @@ window.addEventListener('load', function() {
                         position: {x:0, y:0, z:0},
                         rotation:{ x:0, y:-60, z:0 },
                         material:{ type: 'phong', color:0xF3F3F3 } }, 
-                    { poi_id: 'escola_miramar' });                    
+                    { poi_id: 'escola_miramar' });        
+                   
+                    awe.projections.add({ 
+                        id:'id_escola_miramar3', 
+			geometry:{ shape:'cube', x:50, y:50, z:50 }, 
+                        rotation:{ x:30, y:30, z:0 },
+			material:{ type: 'phong', color: 0xFFFFFF }, 
+                        texture: { path: 'pic.png' },
+                    }, { poi_id: 'escola_miramar' });                    
                     
                     //var degFmt = 'latlon-degree-format';
                     //var degFmt = 'dms';
@@ -246,7 +254,19 @@ window.addEventListener('load', function() {
                         position: {x:0, y:0, z:0},                         
                         rotation:{ x:0, y:-60, z:0 },
                         material:{ type: 'phong', color:0xF3F3F3 } }, 
-                    { poi_id: 'decathlon_vilaseca' });                    
+                    { poi_id: 'decathlon_vilaseca' });                      
+                    
+                    awe.projections.add({ 
+                        id:'id_decathlon_vilaseca3', 
+			geometry:{ shape:'cube', x:50, y:50, z:50 }, 
+                        rotation:{ x:30, y:30, z:0 },
+			material:{ type: 'phong', color: 0xFFFFFF }, 
+                        texture: { path: 'pic.png' },
+                    }, { poi_id: 'decathlon_vilaseca' });
+                    
+                    	
+
+			        
                     
                     //if (new_place){
                     //awe.povs.update({ data: { position: { x: pos.lat*100000, y: 0, z: pos.lng*100000 } }, where: { id: 'default' } });
