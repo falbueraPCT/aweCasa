eee36cb4d2c1e8f3750d589f3e129b0beead0b1b
falbueraPCT <francesc.albuera@pct-turisme.cat>
2018-11-27 13:59:06 +0100

    fasdf

--- examples/basic_GEO/controlador.js
+++ examples/basic_GEO/controlador.js
@@ -223,6 +223,10 @@ look_at($xyz)
 */
                             awe.povs.update({ data: { position: { x: xyz_user[0], y: 0, z: -1 * xyz_user[1] } }, where: { id: 'default' } });                                
 
+                            var test11 = awe.projections.view('id_escola_miramar2').get_mesh().children[0];
+                            var test12 = awe.projections.view('id_escola_miramar2').get_mesh().children[1];
+                            var test13 = awe.projections.view('id_escola_miramar2').get_mesh().children[2];
+                            
                             projection_child = awe.projections.view('id_escola_miramar2').get_mesh().children[0];
                             projection_params = projection_child.geometry.parameters.parameters;
                             projection_child.geometry = new THREE.TextGeometry( (dist1/1000).toPrecision(2) + " KM", projection_params );
